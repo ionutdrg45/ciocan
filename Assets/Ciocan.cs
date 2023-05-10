ce6a7ede72b0cd8f6e97b643ec6d59ddd1245de1
Ionuț Drăgan <draganionut307@gmail.com>
2023-05-10 14:01:46 +0300

    init

---
+++ Assets/Ciocan.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ciocan : MonoBehaviour
+{
+    float rotateSpeed = 50f; 
+    float rotateAngle = 90f; 
+
+  
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            StartCoroutine(Lovire());
+        }
+    }
+
+    IEnumerator Lovire()
+    {
+        float angle = 0f;
+        float time = 0.5f; 
+
+        while (angle < rotateAngle)
+        {
+            angle += rotateSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime); 
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(time - rotateAngle / rotateSpeed); 
+
+        while (angle > 0f)
+        {
+            angle -= rotateSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.left, rotateSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
